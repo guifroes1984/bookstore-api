23e93aa2fa9ee249196a81cec36031c4305e0dcf
guifroes1984 <guilhermefroes26@gmail.com>
2023-03-10 10:30:28 -0300

    instancia de forma automatica da base de dados

--- src/main/java/com/guilherme/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/guilherme/bookstore/repositories/LivroRepository.java
@@ -1,5 +1,11 @@
 package com.guilherme.bookstore.repositories;
 
-public class LivroRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guilherme.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
 
 }
